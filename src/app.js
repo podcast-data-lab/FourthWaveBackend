e4bcb131ca31b2fc235be6645bf0934325515a5b
lbugasu <lbugasu@gmail.com>
2021-02-07 10:45:09 -0500

    Adding Cors to backend

--- src/app.js
+++ src/app.js
@@ -46,9 +46,9 @@ app.use((req, res, next) => {
   ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
+    console.log(origin);
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
   res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", true);
