c422d4e304810a13ab069742164d493024b0d29d
lbugasu <lbugasu@gmail.com>
2022-04-24 10:08:07 -0500

    chore: update schema file

--- src/db/authentication.ts
+++ src/db/authentication.ts
@@ -1,5 +1,4 @@
 import { GraphQLError } from 'graphql'
-import jwt from 'jsonwebtoken'
 import { ObjectId } from 'mongodb'
 import { firebaseAuth } from '../lib/firebase'
 import { UserModel } from '../models'
@@ -7,7 +6,6 @@ import { LibraryModel } from '../models/Library'
 import { User } from '../models/User'
 import { captureException } from '@sentry/node'
 import { UserContext } from '../models/Context'
-// let conf = require("dotenv").config("../../").parsed;
 
 /**
  * A function to authenticate a user
@@ -26,62 +24,6 @@ export const signInOrCreateUser = async (uid: string, email: string): Promise<Us
         await user.save()
     }
     return user
-    let usr: User[] = await UserModel.aggregate([
-        { $match: { uid } },
-        {
-            $lookup: {
-                from: 'plays',
-                foreignField: '_id',
-                localField: 'queue',
-                as: 'queue',
-            },
-        },
-        {
-            $lookup: {
-                from: 'podcasts',
-                foreignField: '_id',
-                localField: 'subscribedPodcasts',
-                as: 'subscribedPodcasts',
-            },
-        },
-
-        {
-            $lookup: {
-                from: 'podcasts',
-                foreignField: '_id',
-                localField: 'likedPodcasts',
-                as: 'likedPodcasts',
-            },
-        },
-        {
-            $lookup: {
-                from: 'episodes',
-                foreignField: '_id',
-                localField: 'likedEpisodes',
-                as: 'likedEpisodes',
-            },
-        },
-
-        {
-            $lookup: {
-                from: 'episodes',
-                foreignField: '_id',
-                localField: 'bookmarkedEpisodes',
-                as: 'bookmarkedEpisodes',
-            },
-        },
-        {
-            $project: {
-                password: 0,
-            },
-        },
-    ])
-    // usr[0].queue = usr[0].queue.sort((a: Play, b: Play) => {
-    //     return userQueue.indexOf(a._id) - userQueue.indexOf(b._id)
-    // })
-    console.log(usr[0].queue)
-
-    return usr[0]
 }
 
 /**
@@ -116,21 +58,5 @@ export const verifyTokenAndGetUser = async (token: string): Promise<Omit<UserCon
     } catch (e) {
         captureException(e)
         throw new Error(e)
-    }
-}
-
-/**
- * Retrieves an authenticated user if a token is valid
- * @param token
- */
-export const getAuthenticatedUser = async (token: String) => {
-    try {
-        let decoded = jwt.verify(token, process.env.JWT_SECRET)
-        let user = await UserModel.find({ email: decoded.email })
-        return user
-    } catch (error) {
-        console.log(error.message)
-        captureException(error)
-        return null
     }
 }
