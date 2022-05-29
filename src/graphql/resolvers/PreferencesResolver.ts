db1177fab9d6cb02f09fdb22e3113f6077e0c8f7
lbugasu <lbugasu@gmail.com>
2022-05-29 16:58:24 -0500

    feat: get user preference query

--- src/graphql/resolvers/PreferencesResolver.ts
+++ src/graphql/resolvers/PreferencesResolver.ts
@@ -1,11 +1,17 @@
-import { Arg, Ctx, Mutation, Resolver } from 'type-graphql'
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql'
 import { UserContext } from '../../models/Context'
 import { UserPreference, UserPreferenceModel } from '../../models/Preference'
 import { DocumentType } from '@typegoose/typegoose'
 
 @Resolver((of) => UserPreference)
 export class PreferencesResolver {
-    @Mutation((returns) => UserPreference, { description: 'Sets a user Volume' })
+    @Query((returns) => UserPreference, { description: "Gets a user' preference." })
+    async getUserPreferences(@Arg('volume') volume: number, @Ctx() { user }: UserContext): Promise<UserPreference> {
+        let preferences = await UserPreferenceModel.findById<DocumentType<UserPreference>>({ _id: user.preferences })
+        return preferences
+    }
+
+    @Mutation((returns) => UserPreference, { description: 'Sets a user Volume.' })
     async setUserVolume(@Arg('volume') volume: number, @Ctx() { user }: UserContext): Promise<UserPreference> {
         let preferences = await UserPreferenceModel.findById<DocumentType<UserPreference>>({ _id: user.preferences })
         preferences.speed = volume
@@ -13,7 +19,7 @@ export class PreferencesResolver {
     }
 
     @Mutation((returns) => UserPreference, {
-        description: "Changes a user's playing speed",
+        description: "Changes a user's playing speed.",
     })
     async changePlayingSpeed(@Arg('speed') speed: number, @Ctx() { user }: UserContext): Promise<UserPreference> {
         let preferences = await UserPreferenceModel.findById<DocumentType<UserPreference>>({ _id: user.preferences })
