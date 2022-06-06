5d39fd10e264a8370cc4a979f331a9f389be3d7f
lbugasu <lbugasu@gmail.com>
2022-06-06 17:15:39 -0500

    feat: edit category featureness

--- src/graphql/resolvers/CategoryResolver.ts
+++ src/graphql/resolvers/CategoryResolver.ts
@@ -1,4 +1,4 @@
-import { Arg, Query, Resolver } from 'type-graphql'
+import { Arg, Mutation, Query, Resolver } from 'type-graphql'
 
 import { Category, CategoryModel } from '../../models/Category'
 
@@ -45,21 +45,40 @@ export default class CategoryResolver {
         return categories
     }
 
-    @Query((returns) => [Category])
-    async getFullCategory(): Promise<Category[]> {
-        const categories = await CategoryModel.aggregate([
-            {
-                $match: { featured: true },
-            },
-            {
-                $lookup: {
-                    from: 'podcasts',
-                    foreignField: '_id',
-                    localField: 'podcast',
-                    as: 'podcast',
-                },
-            },
-        ])
-        return categories
+    @Query((returns) => Category)
+    async getFullCategory(@Arg('categoryId') categoryId: string): Promise<Category> {
+        return getFullCategory(categoryId)
     }
+
+    @Mutation((returns) => Category)
+    async editCategoryFeatureness(@Arg('categoryId') categoryId: string, @Arg('featured') featured: boolean): Promise<Category> {
+        const category = await CategoryModel.findById(categoryId)
+        if (!category) {
+            throw new Error('Category not found')
+        }
+        category.featured = featured
+        await category.save()
+
+        return getFullCategory(categoryId)
+    }
+}
+
+async function getFullCategory(categoryId: string) {
+    let categories = await CategoryModel.aggregate([
+        {
+            $match: { _id: categoryId },
+        },
+        {
+            $lookup: {
+                from: 'podcasts',
+                foreignField: '_id',
+                localField: 'podcast',
+                as: 'podcast',
+            },
+        },
+    ])
+    if (categories.length > 0) {
+        return categories[0]
+    }
+    return null
 }
